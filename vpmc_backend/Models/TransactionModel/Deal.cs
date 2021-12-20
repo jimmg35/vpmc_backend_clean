dd87257f41727ee19fd38831f1260a71688aadbd
MSI\Jim <jim60308@gmail.com>
2021-12-20 13:19:27 +0800

    還沒做完

--- vpmc_backend/Models/TransactionModel/Deal.cs
+++ vpmc_backend/Models/TransactionModel/Deal.cs
@@ -7,5 +7,63 @@ namespace vpmc_backend.Models.TransactionModel
 {
     public class Deal
     {
+
+        
+        /*public string Id { get; set; }                       // "編號"
+        public string? nonMetroLandUseDistrict { get; set; } // "非都市土地使用分區",
+        public int? landTransAmount { get; set; }//"土地交易筆數",
+        public int? buildingTransAmount { get; set; }//"建物交易筆數",
+        public int? parkTransAmount { get; set; }//"車位交易筆數",
+        public int? totalFloorNumber { get; set; }//"總樓層數",
+        
+        public string? buildingMaterial { get; set; }
+        public string? transactionSign { get; set; }
+        public string? nonMetroLandUse { get; set; }
+        public string? transactionDate { get; set; }
+        public string? completionDate { get; set; }
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        public string?
+        "主要建材",
+        "交易標的",
+        "非都市土地使用編定",
+        "交易年月日",
+        "建築完成年月",
+        "移轉層次",
+        "解析移轉層次",
+        "建物型態",
+        "車位類別",
+        "主要用途",
+        "土地位置/建物門牌",
+        "都市土地使用分區",
+        "鄉鎮市區",
+        "備註",
+        "建物移轉總面積(平方公尺)",
+        "土地移轉總面積(平方公尺)",
+        "主建物面積",
+        "車位移轉總面積(平方公尺)",
+        "附屬建物面積",
+        "車位總價(元)",
+        "陽台面積",
+        "總價(元)",
+        "單價(元/平方公尺)",
+        "建物現況格局-衛",
+        "建物現況格局-房",
+        "建物現況格局-廳",
+        "有無管理組織",
+        "建物現況格局-隔間",
+        "電梯",
+        "交易標的橫坐標",	
+        "交易標的縱坐標" */
+        
     }
 }
