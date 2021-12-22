c9a01e0855e198fafbc2c968bd40d4d12f390063
蔡詠名 <r1111r11@hotmail.com>
2021-12-22 11:04:36 +0800

    更新實價登錄交易Models

--- vpmc_backend/Migrations/20211222030207_addDeal2.Designer.cs
+++ vpmc_backend/Migrations/20211222030207_addDeal2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vpmc_backend.Models;
 
 namespace vpmc_backend.Migrations
 {
     [DbContext(typeof(WebApiContext))]
-    partial class WebApiContextModelSnapshot : ModelSnapshot
+    [Migration("20211222030207_addDeal2")]
+    partial class addDeal2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ vpmc_backend/Migrations/20211222030207_addDeal2.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vpmc_backend.Migrations
+{
+    public partial class addDeal2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalPrice",
+                table: "Deal_Manage",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ParkTotalPrice",
+                table: "Deal_Manage",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalPrice",
+                table: "Deal",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ParkTotalPrice",
+                table: "Deal",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalPrice",
+                table: "Deal_Manage",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkTotalPrice",
+                table: "Deal_Manage",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalPrice",
+                table: "Deal",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkTotalPrice",
+                table: "Deal",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+    }
+}
