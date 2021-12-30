a5791b49f2107d385423955453a5773cf93784dc
yungming0119 <77847303+yungming0119@users.noreply.github.com>
2021-12-30 18:24:44 +0800

    新增地區整體實價交易

--- vpmc_backend/Controllers/TransactionController.cs
+++ vpmc_backend/Controllers/TransactionController.cs
@@ -8,6 +8,8 @@ using vpmc_backend.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace vpmc_backend.Controllers
@@ -52,6 +54,55 @@ namespace vpmc_backend.Controllers
                 }
                 ).ToList();
             return result;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("getRegionHistory")]
+        public Dictionary<string, Dictionary<string, Dictionary<string, double>>> GetRegion(string county,string town)
+        {
+            var records = _context.Deal_Manage
+                .Where(x => x.County == county && x.Town == town)
+                .GroupBy(
+                    x => new
+                    {
+                        state = x.BuildingState,
+                        year = x.CompletionDate.Substring(0,4)
+                    })
+                .Select(y => new
+                    {
+                        state = y.Key.state,
+                        year = int.Parse(y.Key.year) - 1911,
+                        counts = y.Count(),
+                        totalPrice = y.Average(x => x.TotalPrice),
+                        unitPrice = y.Average(x => x.UnitPrice),
+                        age = (DateTime.Now.Year - 1911) - (int.Parse(y.Key.year)-1911)
+                    });
+
+            var results = new Dictionary<string, Dictionary<string, Dictionary<string, double>>> ();
+
+            foreach (var record in records)
+            {
+                if (!results.ContainsKey(record.state))
+                {
+                    results.Add(record.state, new Dictionary<string, Dictionary<string, double>>
+                    {
+                        ["counts"] = new Dictionary<string, double>(),
+                        ["totalPrice"] = new Dictionary<string, double>(),
+                        ["unitPrice"] = new Dictionary<string, double>(),
+                        ["age"] = new Dictionary<string, double>()
+                    });
+                }
+
+                results[record.state]["counts"].Add(record.year.ToString(),record.counts);
+                results[record.state]["totalPrice"].Add(record.year.ToString(), (double)record.totalPrice);
+                results[record.state]["unitPrice"].Add(record.year.ToString(), (double)record.unitPrice);
+                results[record.state]["age"].Add(record.year.ToString(), record.age);
+            }
+
+            //var options = new JsonSerializerOptions { WriteIndented = true };
+            //string jsonString = JsonSerializer.Serialize(results, options);
+
+            return results;
         }
     }
 
